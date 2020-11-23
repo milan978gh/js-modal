f15cc87620bf2c7a18daa882196b750daaf62282
milan978gh <amilandev78@gmail.com>
2020-11-23 19:21:27 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const modalBtn = document.querySelector('.modal-btn');
-const overlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
 
 modalBtn.addEventListener('click', function () {
-  overlay.classList.toggle('open-modal');
+  modal.classList.toggle('open-modal');
 });
 closeBtn.addEventListener('click', function () {
-  overlay.classList.remove('open-modal');
+  modal.classList.remove('open-modal');
 });
 
